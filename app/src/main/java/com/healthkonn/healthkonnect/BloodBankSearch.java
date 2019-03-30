b53022b747ae61a29bfac1d12397bc061d11a74e
Gunjan Bendale <gunjan.bendale@gmail.com>
2019-03-30 16:05:12 +0530

    Initial commit

---
+++ app/src/main/java/com/healthkonn/healthkonnect/BloodBankSearch.java
@@ -0,0 +1,13 @@
+package com.healthkonn.healthkonnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BloodBankSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blood_bank_search);
+    }
+}
